cd58488220ae391ee812a996dc7bfd9b851e13d7
Jacob, Roling <jacob.roling@hey.com>
2024-04-16 17:24:33 +0930

    chore: savepoint

--- src/parser.rs
+++ src/parser.rs
@@ -4,35 +4,51 @@ use thiserror::Error;
 
 use crate::{lexer::Token, node::Node};
 
-pub struct Parser {
-    tokens: Receiver<Token>,
-}
-
 #[derive(Error, Debug)]
 pub enum ParserError {
     #[error("test")]
     Test,
 }
 
+pub struct Parser {
+    tokens: Receiver<Token>,
+    buffer: Vec<Token>,
+    start_position: usize,
+    position: usize,
+}
+
 impl Parser {
-    pub fn new(tokens: Receiver<Token>) -> Self {
-        return Self { tokens };
+    fn new(tokens: Receiver<Token>) -> Self {
+        return Self {
+            tokens,
+            buffer: Vec::new(),
+            start_position: 0,
+            position: 0,
+        };
     }
 
-    pub fn next(&self) -> Token {
-        return match self.tokens.recv() {
-            Ok(token) => token,
-            Err(e) => panic!("{}", e),
-        };
+    fn next(&mut self) -> Option<&Token> {
+        if let Ok(token) = self.tokens.recv() {
+            self.buffer.push(token);
+        }
+
+        if self.position >= self.buffer.len() {
+            return None;
+        }
+
+        let token = &self.buffer[self.position];
+        self.position += 1;
+
+        return Some(token);
     }
 }
 
 pub fn parse(token_reciever: Receiver<Token>) -> Result<Node, ParserError> {
-    let parser = Parser::new(token_reciever);
+    let mut parser = Parser::new(token_reciever);
 
     let children = Vec::new();
 
-    while let Ok(token) = parser.tokens.recv() {
+    while let Some(token) = parser.next() {
         println!("{:#?}", token);
     }
 
